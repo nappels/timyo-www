d73cabf098f188291da1b460350aa1c126a3c57a
Noah Appel <noah.appel@gmail.com>
2017-05-18 13:57:25 +0200

    Mobile nav styles.

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -2,9 +2,27 @@
 // Main JavaScript file
 // --------------------------------------------------------
 
+
+// Event handlers
+var handlers = {
+  toggleNavbar: function() {
+    document.body.classList.toggle('mobile-nav-open');
+  }
+};
+
 // Header scrolling
 (function header(w) {
-  w.addEventListener('scroll', function() {
+  var toggleScrolledClass = function() {
     document.body.classList.toggle('scrolled', w.pageYOffset >= 16);
-  });
+  };
+  w.addEventListener('scroll', toggleScrolledClass);
+  toggleScrolledClass();
+})(window);
+
+// Event binding
+(function events(w) {
+  var clickEventEls = document.querySelectorAll('[data-event-click]');
+  for (var i = 0; i < clickEventEls.length; i++) {
+    clickEventEls[i].addEventListener('click', w.handlers[clickEventEls[i].dataset.eventClick]);
+  }
 })(window);
