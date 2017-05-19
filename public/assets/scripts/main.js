43a77034c3d10762d0e81d9d97b1b83e22d995ec
Noah Appel <noah.appel@gmail.com>
2017-05-19 14:55:14 +0200

    screenshot section of homepage. set up tabbing methods.

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -3,26 +3,44 @@
 // --------------------------------------------------------
 
 
-// Event handlers
-var handlers = {
-  toggleNavbar: function() {
-    document.body.classList.toggle('mobile-nav-open');
+var Timyo = {
+  init: function() {
+    window.addEventListener('scroll', Timyo.events.toggleScrolledClass);
+    Timyo.events.toggleScrolledClass();
+    Timyo.bindEvents();
+  },
+  bindEvents: function() {
+    var clickEventEls = document.querySelectorAll('[data-event-click]');
+    for (var i = 0; i < clickEventEls.length; i++) {
+      clickEventEls[i].addEventListener('click', Timyo.events[clickEventEls[i].dataset.eventClick]);
+    }
+  },
+  events: {
+    toggleNavbar: function() {
+      document.body.classList.toggle('mobile-nav-open');
+    },
+    handleHomepageTabClick: function(e) {
+      Timyo.events.setActiveCard(e);
+      Timyo.events.setActiveHomepageScreen(e);
+    },
+    setActiveCard: function(e) {
+      for (var card of e.currentTarget.parentNode.childNodes) {
+        card.classList.remove('active');
+      }
+      e.currentTarget.classList.add('active');
+    },
+    setActiveHomepageScreen: function(e) {
+      var screenWrapper = document.querySelector('.screen-wrapper');
+      var id = e.currentTarget.dataset.tabId;
+      for (var screenEl of screenWrapper.childNodes) {
+        screenEl.classList.remove('active');
+      }
+      screenWrapper.querySelector('[data-id="' + id + '"]').classList.add('active');
+    },
+    toggleScrolledClass: function() {
+      document.body.classList.toggle('scrolled', window.pageYOffset >= 16);
+    },
   }
 };
 
-// Header scrolling
-(function header(w) {
-  var toggleScrolledClass = function() {
-    document.body.classList.toggle('scrolled', w.pageYOffset >= 16);
-  };
-  w.addEventListener('scroll', toggleScrolledClass);
-  toggleScrolledClass();
-})(window);
-
-// Event binding
-(function events(w) {
-  var clickEventEls = document.querySelectorAll('[data-event-click]');
-  for (var i = 0; i < clickEventEls.length; i++) {
-    clickEventEls[i].addEventListener('click', w.handlers[clickEventEls[i].dataset.eventClick]);
-  }
-})(window);
+Timyo.init();
