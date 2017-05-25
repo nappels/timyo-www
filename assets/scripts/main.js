3ad4378427c60aa73582150d1f1a532022f9ca73
Noah Appel <noah.appel@gmail.com>
2017-05-25 17:39:30 +0200

    Video player modal and content updates

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,8 +6,10 @@
 var Timyo = {
   init: function() {
     window.addEventListener('scroll', Timyo.events.toggleScrolledClass);
+    window.addEventListener('keydown', Timyo.events.handleEscClick);
     Timyo.events.toggleScrolledClass();
     Timyo.bindEvents();
+    Timyo.sliders.init();
   },
   bindEvents: function() {
     var clickEventEls = document.querySelectorAll('[data-event-click]');
@@ -15,34 +17,103 @@ var Timyo = {
       clickEventEls[i].addEventListener('click', Timyo.events[clickEventEls[i].dataset.eventClick]);
     }
   },
+  sliders: {
+    init: function() {
+      Swipe(Timyo.sliders.testimonials.el, Timyo.sliders.testimonials.options);
+      Swipe(Timyo.sliders.screenshots.el, Timyo.sliders.screenshots.options);
+    },
+    screenshots: {
+      el: document.getElementById('screenshot-slider'),
+      options: {
+        callback: function(index) {
+          Timyo.events.setActiveCard(index);
+          Timyo.events.setActiveHomepageScreen(index);
+        }
+      }
+    },
+    testimonials: {
+      el: document.getElementById('testimonial-slider'),
+      options: {
+        auto: 6000,
+        callback: function(index) {
+          Timyo.events.setTestimonialActiveIndex(index);
+        }
+      }
+    }
+  },
   events: {
     toggleNavbar: function() {
       document.body.classList.toggle('mobile-nav-open');
     },
     handleHomepageTabClick: function(e) {
-      Timyo.events.setActiveCard(e);
-      Timyo.events.setActiveHomepageScreen(e);
+      var index = e.currentTarget.dataset.index;
+      Timyo.events.setActiveCard(index);
+      Timyo.events.setActiveHomepageScreen(index);
     },
-    setActiveCard: function(e) {
-      var children = Array.prototype.slice.call(e.currentTarget.parentNode.childNodes);
-      for (var i = 0; i < children.length; i++ ) {
-        children[i].classList.remove('active');
+    setActiveCard: function(index) {
+      var cards = document.querySelectorAll('#screenshot-slider .icon-card');
+
+      for (var i = 0; i < cards.length; i++ ) {
+        cards[i].classList.remove('active');
+        if (cards[i].dataset.index == index) {
+          cards[i].classList.add('active');
+        }
       }
-      e.currentTarget.classList.add('active');
     },
-    setActiveHomepageScreen: function(e) {
-      var screenWrapper = document.querySelector('.screen-wrapper');
-      var id = e.currentTarget.dataset.tabId;
-      var children = Array.prototype.slice.call(screenWrapper.childNodes);
-      for (var i = 0; i < children.length; i++ ) {
-        children[i].classList.remove('active');
+    setActiveHomepageScreen: function(index) {
+      var screens = document.querySelectorAll('.screen-wrapper .screen');
+
+      for (var i = 0; i < screens.length; i++ ) {
+        screens[i].classList.remove('active');
+        if (screens[i].dataset.index == index) {
+          screens[i].classList.add('active');
+        }
       }
-      screenWrapper.querySelector('[data-id="' + id + '"]').classList.add('active');
     },
     toggleScrolledClass: function() {
       document.body.classList.toggle('scrolled', window.pageYOffset >= 16);
     },
+    toggleModal: function() {
+      if (document.body.classList.contains('modal-open')) {
+        player.pauseVideo();
+      }
+      document.body.classList.toggle('modal-open');
+
+
+    },
+    setTestimonialActiveIndex: function(index) {
+      var dots = document.querySelectorAll('.testimonials-section .dot-wrapper .dot');
+
+      var activeIndex = index;
+
+      if (index > dots.length - 1) activeIndex = index - dots.length;
+
+      for (var i = 0; i < dots.length; i++ ) {
+        var currentDot = dots[i];
+        currentDot.classList.remove('active');
+        if (currentDot.dataset.index == activeIndex) {
+          currentDot.classList.add('active');
+        }
+      }
+    },
+    handleEscClick: function(e) {
+      if (e.keyCode === 27 && document.body.classList.contains('modal-open')) {
+        document.body.classList.remove('modal-open');
+      }
+
+      return false;
+    }
   }
 };
 
 Timyo.init();
+
+// Global YouTube video mounting
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '560',
+    width: '315',
+    videoId: 'ONXw1IVdAcc'
+  });
+}
